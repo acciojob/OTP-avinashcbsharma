adf18fb6db559df6cc6db627c89ca2ebcc3ab59c
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-20 21:42:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-window.onload = function() {
-    document.getElementsByClassName("code-container").focus();
+function handleInput(currentInput) {
+    const maxLength = parseInt(currentInput.getAttribute('maxlength'));
+    const currentInputId = currentInput.id;
+
+    if (currentInput.value.length === maxLength) {
+      
+        const nextInputId = "digit" + (parseInt(currentInputId.charAt(currentInputId.length - 1)) + 1);
+        document.getElementById(nextInputId)?.focus();
+    } else if (currentInput.value.length === 0 && event.inputType === 'deleteContentBackward') {
+      
+        const prevInputId = "digit" + (parseInt(currentInputId.charAt(currentInputId.length - 1)) - 1);
+        document.getElementById(prevInputId)?.focus();
+    }
+}
